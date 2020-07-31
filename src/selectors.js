614b3263e805f06621791e4601714aa369ade0f0
Ivaylo Ivanov <ivailo.ivov.ivanov@gmail.com>
2020-07-31 08:52:49 +0300

    remove unused selectors file

--- src/selectors.js
+++
@@ -1,7 +0,0 @@
-import { createSelector } from 'reselect';
-const getUsers = (state) => state.users;
-
-export const userSelector = createSelector(
-    getUsers, 
-    (users, userId) => { return users.find(x => x.id == userId) }
-);
